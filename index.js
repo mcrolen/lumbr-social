2a558e13ac8d0008b12338b986ed8a15e3595df4
mcrolen <>
2021-04-28 00:59:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('express-flash');
+const connectDB = require('./config/db');
+
+// Bring in routes
+const mainRoutes = require('./routes/main');
+const postRoutes = require('./routes/posts');
+
+dotenv.config({ path: './config/config.env'});
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 2121;
+
+// Call routes
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+
+// Run server
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
