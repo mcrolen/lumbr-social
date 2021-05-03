02bbea01e3f3d10e6d8d210323e4865287807e0f
Reginald Traywick <reggie@hey.com>
2021-05-03 14:10:21 -0700

    server will start and login page works

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,20 @@
-const express = require('express') 
-const router = express.Router()
-const postsController = require('../controllers/posts')
-const authController = require('../controllers/auth') 
-const homeController = require('../controllers/home') 
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
- 
-router.get('/', homeController.getIndex)
+const express = require("express");
+const router = express.Router();
+const postsController = require("../controllers/posts");
+const authController = require("../controllers/auth");
+const homeController = require("../controllers/home");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-router.get('/login', ensureAuth, authController.getLogin)
-router.post('/login', authController.postLogin) // Why do you need a POST request here?
-router.get('/logout', authController.logout)
+router.get("/", homeController.getIndex);
 
-router.get('/signup', ensureAuth, authController.getSignup)
-router.post('/signup', authController.postSignup) // Why do you need a POST request here?
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
 
-router.get('/profile', postsController.getLogin)
-router.get('/feed', postsController.getFeed)
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
-module.exports = route
+router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/feed", ensureAuth, postsController.getFeed);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,64 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const flash = require('express-flash');
-const connectDB = require('./config/db');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/db");
 
 // Bring in routes
-// const mainRoutes = require('./routes/main');
-// const postRoutes = require('./routes/posts');
+const mainRoutes = require("./routes/main.js");
+const postRoutes = require("./routes/posts.js");
 
-dotenv.config({ path: './config/config.env' });
-const app = express();
+// Use the .env file in config folder
+require("dotenv").config({ path: "./config/config.env" });
+
+// Passport
+require("./config/passport.js")(passport);
+
+// Connect to the database using db string in config folder
 connectDB();
 
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static
+app.use(express.static("public"));
+
+// Use EJS for the contents of views folder
+app.set("view engine", "ejs");
+
+// Using express built in magic for body parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Logging with Morgan
+app.use(logger("dev"));
+
+// Put and delete requests directly in forms
+app.use(methodOverride("_method"));
+
+// Sessions stored in MongoDB
 app.use(
   session({
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
-// // Call routes
-// app.use('/', mainRoutes);
-// app.use('/post', postRoutes);
+// Use flash for errors
+app.use(flash());
+
+// Call routes
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
 
 const PORT = process.env.PORT || 2121;
 
